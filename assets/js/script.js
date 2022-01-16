de3ea7b2ece0fd1d52711a466cf6bf2f1240b1fa
cedean1029 <cedean1029@gmail.com>
2022-01-16 18:37:59 -0500

    final edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,17 @@ function generatePassword() {
   console.log(passwordString.length)
   return passwordString
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  currentPassword = password
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
